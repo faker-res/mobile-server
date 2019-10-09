486beca7922d96df049adaf4950a7a42602cf329
robert <123456@123.com>
2019-10-09 19:18:57 +0800

    Fix：水浒传排行榜

--- src/main/java/com/lzkj/mobile/config/GameMongoDB.java
+++ src/main/java/com/lzkj/mobile/config/GameMongoDB.java
@@ -1,5 +1,6 @@
 package com.lzkj.mobile.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 
+@Slf4j
 @Configuration
 @ConfigurationProperties(prefix="spring.data.mongodb.game")
 public class GameMongoDB {
@@ -18,6 +20,7 @@ public class GameMongoDB {
 
     @Bean(name="gameMongoTemplate")
     public MongoTemplate getMongoTemplate() {
+        log.info("---------------------------------mongoDB config-----------------------------------"+url);
         return new MongoTemplate(new SimpleMongoDbFactory(new MongoClientURI(url,MongoClientOptions.builder().maxConnectionIdleTime(30000).connectTimeout(5000))));
     }
 }
