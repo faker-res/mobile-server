2891380074fc95252b919bb5588e4a02bc66073f
web <web@DESKTOP-DM1M875>
2019-11-12 16:25:27 +0800

    支付主备域名切换

---
+++ src/main/java/com/lzkj/mobile/vo/DomainInfoVO.java
@@ -0,0 +1,16 @@
+package com.lzkj.mobile.vo;
+
+
+import lombok.Data;
+
+@Data
+public class DomainInfoVO {
+	
+	private String sendUrl;
+	
+	private Integer sort;
+	
+	private Integer id;
+	
+	private Integer type;
+}
