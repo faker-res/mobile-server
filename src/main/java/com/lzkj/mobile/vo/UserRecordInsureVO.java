60b10279189f7c4739ca6f02e8f6e030644becb9
logan <1232121212456@qq.com>
2019-11-26 14:47:31 +0800

    余额宝明细接口

--- src/main/java/com/lzkj/mobile/vo/UserRecordInsureVO.java
+++ src/main/java/com/lzkj/mobile/vo/UserRecordInsureVO.java
@@ -6,8 +6,8 @@ import lombok.Data;
 @Data
 public class UserRecordInsureVO {
 
-	private String CollectDate;
-	private Integer TradeType;
-	private BigDecimal SwapScore;
-	private BigDecimal VarietyScore;
+	private String dateTime;
+	private Integer tradeType;
+	private BigDecimal changeScore;
+	private BigDecimal beforeScore;
 }
