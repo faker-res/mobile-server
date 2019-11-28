f4088d0c7d54b15827e1c1bb1693f726f1168d3c
robert <123456@123.com>
2019-11-28 15:14:07 +0800

    FIX:领取洗码金额发金币信息给协调服

--- src/main/java/com/lzkj/mobile/controller/CleanChipsController.java
+++ src/main/java/com/lzkj/mobile/controller/CleanChipsController.java
@@ -1,8 +1,11 @@
 package com.lzkj.mobile.controller;
 
 import com.lzkj.mobile.client.PlatformServiceClient;
+import com.lzkj.mobile.util.HttpRequest;
 import com.lzkj.mobile.vo.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +15,15 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/clean")
+@Slf4j
 public class CleanChipsController {
 
 
     @Autowired
     private PlatformServiceClient platformServiceClient;
+
+    @Value("${server.url}")
+    private String serverUrl;
 
     /**
      * 1.视讯，2.电子，3.棋牌,4.捕鱼,5.体育,6.彩票
@@ -45,9 +52,17 @@ public class CleanChipsController {
         if (vipLevel==0){
             vipLevel=1;
         }
-        Boolean flag = platformServiceClient.washBet(userId,agentId,vipLevel);
+        Map <String ,Object> param= platformServiceClient.washBet(userId,agentId,vipLevel);
         GlobeResponse globeResponse = new GlobeResponse();
-        globeResponse.setData(flag);
+        if (true ==(Boolean) param.get("flag")) {
+            Object score = param.get("score");
+            Object insureScore = param.get("insureScore");
+            String msg = "{\"msgid\":7,\"userId\":" + userId + ", \"score\":" + score + ",\"insuranceScore\":" + insureScore +
+                    ", \"VipLevel\":" + vipLevel + ", \"type\":" + 0 + ", \"Charge\":" + 0 + "}";
+            log.info("调用金额变更指令:{}, 返回：" + HttpRequest.sendPost(this.serverUrl, msg), msg);
+        }else{
+            globeResponse.setData(false);
+        }
         return globeResponse;
     }
 
