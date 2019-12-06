cc0a949b25825132e48a31190c7932b5f8fcb9dd
Ball <admin123456>
2019-12-06 16:30:15 +0800

    更新银行卡审核！

--- src/main/java/com/lzkj/mobile/config/AgentSystemEnum.java
+++ src/main/java/com/lzkj/mobile/config/AgentSystemEnum.java
@@ -21,7 +21,8 @@ public enum AgentSystemEnum {
     BANKOPEN("BankOpen","注册时填写银行卡号"),
     REGISTEREDPHONEOPEN("RegisteredPhoneOpen","注册时手机号开关"),
     TXYEBPASSWORDOPEN("TXYEBpasswordOpen","提现时输入余额宝密码开关"),
-    REGISTERACCOUNTOPEN("RegisterAccountOpen","注册帐号开关");
+    REGISTERACCOUNTOPEN("RegisterAccountOpen","注册帐号开关"),
+    YEBPASSWORDOPEN("YEBpasswordOpen","余额宝密码开关");
 
     private String name;
 
