c57ca900a470d1c90a063a6485e3cc349ff2174f
Ball <admin123456>
2019-12-17 12:11:36 +0800

    更新错误日志接口！

--- src/main/java/com/lzkj/mobile/vo/GameException.java
+++ src/main/java/com/lzkj/mobile/vo/GameException.java
@@ -4,10 +4,20 @@ import lombok.Data;
 
 @Data
 public class GameException {
+    private String time;
+    private String yeZhu;
+    private String error;
     private String errorMessage;
     private String file;
     private String line;
     private String message;
-    private String error;
+    private String httpSend;
+    private String scene;
+    private String socketMainCode;
+    private String socketSubCode;
+    private String eventName;
+    private String newHandle;
+    private String kindId;
+    private String platform;
 
 }
