4b2b96e7e9c6eaca8740cf7d94108b6d680e56b6
list <list@example.com>
2019-12-27 12:33:02 +0800

    红包优化

--- src/main/java/com/lzkj/mobile/vo/LoginRedEnvepoleStatusVO.java
+++ src/main/java/com/lzkj/mobile/vo/LoginRedEnvepoleStatusVO.java
@@ -1,9 +1,12 @@
 package com.lzkj.mobile.vo;
 
+import java.math.BigDecimal;
+
 import lombok.Data;
 
 @Data
 public class LoginRedEnvepoleStatusVO {
 
 	private Integer loginRedEnvepoleStatus;
+	private BigDecimal redAmount;
 }
