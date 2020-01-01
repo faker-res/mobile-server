fd7218bc859915b9c8f8916027188fe1837dedc5
Ball <admin123456>
2020-01-01 21:08:29 +0800

    更新公司充值列表！

--- src/main/java/com/lzkj/mobile/client/TreasureServiceClient.java
+++ src/main/java/com/lzkj/mobile/client/TreasureServiceClient.java
@@ -107,6 +107,8 @@ public interface TreasureServiceClient {
                      @RequestParam("payId") Integer payId, @RequestParam("orderAmount") BigDecimal orderAmount, @RequestParam("remarks") String remarks, @RequestParam("account") String account,
                      @RequestParam("paymentAccount") String paymentAccount, @RequestParam("paymentName") String paymentName);
 
+	@RequestMapping("treasure/mobile/getPayId")
+	Integer getPayId(@RequestParam("agentId")Integer agentId, @RequestParam("payName")String payName);
 
 	@RequestMapping("treasure/mobile/getUpdateAddress")
 	GatewayInfo getUpdateAddress(@RequestParam("agentId") Integer agentId);
