7fcfa07f426de9b37d1f1cd08ba8a4c1d0d50b98
dodo <1236456@qq.com>
2020-01-02 17:08:59 +0800

    玩家打码量详情 回滚

--- src/main/java/com/lzkj/mobile/controller/AgentSystemController.java
+++ src/main/java/com/lzkj/mobile/controller/AgentSystemController.java
@@ -732,7 +732,7 @@ public class AgentSystemController {
         }
         GlobeResponse<Object> globeResponse = new GlobeResponse<>();
         //List<UserBetInfoVO>  vo =  accountsClient.getUserBetInfo(userId,agentId);
-        List<UserCodeDetailsVO> param = this.accountsClient.cashFlowDetails(userId, agentId);
+        UserCodeDetailsVO param = this.accountsClient.cashFlowDetails(userId, agentId);
         if (param == null) {
             UserCodeDetailsVO userCodeDetailsVO = new UserCodeDetailsVO();
             //是否可提现状态
@@ -742,12 +742,6 @@ public class AgentSystemController {
             //实际打码量
             userCodeDetailsVO.setCodeAmountCount(BigDecimal.valueOf(0));
             userCodeDetailsVO.setApplyDate(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date().getTime()));
-            //打码类型
-            userCodeDetailsVO.setAccountType(0);
-            //未完成打码量
-            userCodeDetailsVO.setNeedAmount(BigDecimal.valueOf(0));
-            //打码类型名称
-            userCodeDetailsVO.setAccountName("");
             globeResponse.setData(userCodeDetailsVO);
 
 
