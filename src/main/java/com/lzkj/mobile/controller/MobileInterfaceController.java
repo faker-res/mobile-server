07658b3a14adbea185b770ea705db4103894fefb
Ball <admin123456>
2020-01-04 18:23:54 +0800

    更新支付列表！

--- src/main/java/com/lzkj/mobile/controller/MobileInterfaceController.java
+++ src/main/java/com/lzkj/mobile/controller/MobileInterfaceController.java
@@ -1020,31 +1020,31 @@ public class MobileInterfaceController {
         userId = userId == null ? 0 : userId;
         Map<String, List<PayInfoVO>> payList = treasureServiceClient.getPayList(userId,agentId);   //第三方充值渠道
         List<CompanyPayVO> companyList = treasureServiceClient.getCompanyPay(agentId);          //公司充值
-       /* if (companyList != null && companyList.size()>0) {
+        if (companyList != null && companyList.size()>0) {
             companyList.forEach(type -> {
                 if ("AliPay".equals(type.getPayType())) {
-                    type.setId(0);
+                    type.setPayId(0);
                 }
                 if ("WeChatPay".equals(type.getPayType())) {
-                    type.setId(1);
+                    type.setPayId(1);
                 }
                 if ("BankPay".equals(type.getPayType())) {
-                    type.setId(2);
+                    type.setPayId(2);
                 }
                 if ("CloudPay".equals(type.getPayType())) {
-                    type.setId(3);
+                    type.setPayId(3);
                 }
                 if ("QQPay".equals(type.getPayType())) {
-                    type.setId(4);
+                    type.setPayId(4);
                 }
                 if ("JinDongPay".equals(type.getPayType())) {
-                    type.setId(5);
+                    type.setPayId(5);
                 }
                 if("redPwd".equals(type.getPayType())){
-                    type.setId(6);
+                    type.setPayId(6);
                 }
             });
-        }*/
+        }
         Map<String, Object> data = new HashMap<>();
         data.put("payList", payList);
         data.put("compayList", companyList);
