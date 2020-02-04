accd7f5a3e9cc5428d587e7283b4791ed6a2e462
dodo <1741236907@qq.com>
2020-02-04 21:46:27 +0800

    玩家打码量详情

--- src/main/java/com/lzkj/mobile/vo/UserCodeDetailsVO.java
+++ src/main/java/com/lzkj/mobile/vo/UserCodeDetailsVO.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 @Data
 public class UserCodeDetailsVO {
     private Integer userId;
-    /**业主号*/
+    //业主号
     private Integer agentId;
     private Integer userLevel;
     /**时间*/
@@ -19,7 +19,12 @@ public class UserCodeDetailsVO {
     private BigDecimal inAmounts;
     /**实际打码量*/
     private BigDecimal codeAmountCount;
-    /**状态*/
+    /**待完成的打码量*/
+    private BigDecimal needAmountCount;
+    /**打码类型*/
     private Integer status;
-
+    /**打码类型名称*/
+    private String typeName;
+    /**是否可提现：-1表示空；0表示不可提现；1表示可提现*/
+    private Integer flag =-1;
 }
