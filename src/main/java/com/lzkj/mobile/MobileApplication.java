94ec57a176b40c29e7e9772eb99611b4adc2053c
jacob <jacob@qq.com>
2020-02-11 17:19:29 +0800

    * 活动异步方法整合

--- src/main/java/com/lzkj/mobile/MobileApplication.java
+++ src/main/java/com/lzkj/mobile/MobileApplication.java
@@ -2,9 +2,7 @@ package com.lzkj.mobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableScheduling
-//@EnableAsync
+@EnableAsync
 public class MobileApplication {
 
     public static void main(String[] args) {
