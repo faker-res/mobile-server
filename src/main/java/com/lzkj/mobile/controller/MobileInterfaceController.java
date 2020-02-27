85f43ffbc8e8b7bb0632f7ca994296af5d7eb91c
dodo <1741236907@qq.com>
2020-02-27 20:07:56 +0800

    prerelease --修复 报错:因为这两个方法在pay-server 用的是post 请求,所以,一直取不到参数

--- src/main/java/com/lzkj/mobile/controller/MobileInterfaceController.java
+++ src/main/java/com/lzkj/mobile/controller/MobileInterfaceController.java
@@ -1569,19 +1569,42 @@ public class MobileInterfaceController {
         return globeResponse;
     }
 
-    @RequestMapping("/updateMerchantOrderId")
+    @PostMapping("/updateMerchantOrderId")
     public String updateMerchantOrderId(String ownerOrderId, String merchantOrderId, Integer orderStatus) {
-        if (orderStatus == null) {
-            orderStatus = 0;
+        try{
+            //增加参数判断,避免报错
+            if(StringUtils.isBlank(ownerOrderId)){
+                log.info("/updateMerchantOrderId接口参数ownerOrderId为空:{}",ownerOrderId);
+                return "false";
+            }else {
+                if (orderStatus == null) {
+                    orderStatus = 0;
+                }
+                this.treasureServiceClient.updateMerchantOrderId(ownerOrderId, merchantOrderId, orderStatus);
+                return "ok";
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return "false";
         }
-        this.treasureServiceClient.updateMerchantOrderId(ownerOrderId, merchantOrderId, orderStatus);
-        return "ok";
     }
 
-    @RequestMapping("/updatePassagewayResponse")
+    @PostMapping("/updatePassagewayResponse")
     public String updatePassagewayResponse(String ownerOrderId, String passagewayResponse) {
-        this.treasureServiceClient.updatePassagewayResponse(ownerOrderId, passagewayResponse);
-        return "ok";
+        try{
+            //增加参数判断,避免报错
+            if(StringUtils.isBlank(ownerOrderId)){
+                log.info("/updatePassagewayResponse接口参数ownerOrderId为空:{}",ownerOrderId);
+                return "false";
+            }else {
+                this.treasureServiceClient.updatePassagewayResponse(ownerOrderId, passagewayResponse);
+                return "ok";
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return "false";
+        }
+
     }
 
     /**
